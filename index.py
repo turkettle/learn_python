14390c1690b356bd9afb8f655d6976444e4d075c
Gaetan Pralong <contact@gaetan.io>
2016-08-15 20:27:02 +0200

    Modification

--- index.py
+++ index.py
@@ -3,31 +3,3 @@
 import os
 import pickle
 import User
-
-# help(os)
-# path = os.chdir('/home/')
-# print(path)
-
-users = [
-    {
-        "name": 'Gaetan',
-        "age": 36
-    },
-    {
-        "name": 'Elise',
-        "age": 25
-    }
-]
-
-with open('user.db', 'wb') as db_file:
-    db = pickle.Pickler(db_file)
-    db.dump(users)
-
-with open('user.db', 'rb') as db_file:
-    db = pickle.Unpickler(db_file)
-    data = db.load()
-
-# print(data)
-
-for i, element in enumerate(data):
-    print('{} a {} ans'.format(element['name'], str(element['age'])))
